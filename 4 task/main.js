bee35335da116203569add28d2adf759d01468fd
Nikolay <mizenko1998@gmail.com>
2019-05-13 18:05:40 +1000

    add 4.3

--- 4 task/main.js
+++ 4 task/main.js
@@ -27,3 +27,33 @@ form.addEventListener("submit",function(event) {
   }
   event.preventDefault();
 });
+
+form = document.getElementById('task_4_3')
+form.addEventListener("submit",function(event) {
+  let elements = Object.values(event.currentTarget.getElementsByTagName('input'));
+  let M = elements.reduce(function(acc, el, i) {
+    let value = parseFloat(el.value);
+    if (isNaN(value)) {
+      elements[i].classList.add('is-invalid');
+    } else {
+      elements[i].classList.remove('is-invalid');
+    }
+    if (i % 6 === 0) {
+      return [...acc, [value]];
+    } else {
+      return [...acc.slice(0,-1), [...acc[acc.length-1], value]];
+    }
+  }, []);
+
+  let sumOfMajorDiagonal = 0;
+  for (let i = 0; i < 6; i++) {
+    sumOfMajorDiagonal += M[i][i];
+  }
+  console.log(sumOfMajorDiagonal);
+  console.log(M.map(function(cur){
+    return cur.map(function(el){
+      return el - sumOfMajorDiagonal;
+    });
+  }));
+  event.preventDefault();
+});
